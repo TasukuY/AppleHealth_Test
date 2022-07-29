8d6ac41868586824657676a9a9aafa3d339246e1
TasukuY <alohaty81@gmail.com>
2022-07-29 00:52:38 -0600

    Initial Commit

---
+++ AppleHealth_Test/AppleHealth_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleHealth_TestApp.swift
+//  AppleHealth_Test
+//
+//  Created by Tasuku Yamamoto on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct AppleHealth_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
